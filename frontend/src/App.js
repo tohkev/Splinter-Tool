bc5ad0d63f970987977b8e84c9a197574f226737
Kevin <tohkev@gmail.com>
2022-03-25 23:49:45 -0700

    initialized components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-import CardList from "./cards/pages/CardList";
+import CardList from "./cards/components/CardList";
 import "./App.css";
+import MainNav from "./UIElements/Navigation/MainNav";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>App component</h1>
-      <CardList />
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<MainNav />
+			<div className="App">
+				<h1>App component</h1>
+				<CardList />
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default App;
