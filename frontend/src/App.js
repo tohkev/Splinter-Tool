d0ea0ce0f96b7e843ac22212bbfb13585c0b4fe8
Kevin <tohkev@gmail.com>
2022-03-26 10:22:11 -0700

    added initial routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 
-import CardList from "./cards/components/CardList";
-import "./App.css";
 import MainNav from "./UIElements/Navigation/MainNav";
+import CardList from "./cards/components/CardList";
+import UserCards from "./cards/pages/UserCards";
+import "./App.css";
 
 function App() {
+	let routes = (
+		<Routes>
+			<Route path="/" element={<h1>Home Page</h1>} />
+			<Route path="/mycards" element={<UserCards user="snud" />} />
+			<Route path="/:username" element={<UserCards />} />
+			<Route path="*" element={<Navigate to="/" replace />} />
+		</Routes>
+	);
+
 	return (
-		<React.Fragment>
+		<BrowserRouter>
 			<MainNav />
-			<div className="App">
-				<h1>App component</h1>
-				<CardList />
-			</div>
-		</React.Fragment>
+			{routes}
+		</BrowserRouter>
 	);
 }
 
