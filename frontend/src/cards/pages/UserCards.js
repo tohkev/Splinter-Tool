63aa60d29596c59c9503e2b4b5e8b8cfccffb3c4
Kevin <tohkev@gmail.com>
2022-03-27 10:45:18 -0700

    added filter menu, card list, and styling

--- frontend/src/cards/pages/UserCards.js
+++ frontend/src/cards/pages/UserCards.js
@@ -1,5 +1,8 @@
 import React from "react";
+
 import UserCollectionStats from "../components/UserCollectionStats";
+import FilterMenu from "../components/FilterMenu";
+import CardList from "../components/CardList";
 
 import "./UserCards.css";
 
@@ -7,6 +10,10 @@ function UserCards(props) {
 	return (
 		<React.Fragment>
 			<UserCollectionStats user={props.user} />
+			<div className="user-cards__library">
+				<FilterMenu />
+				<CardList />
+			</div>
 		</React.Fragment>
 	);
 }
