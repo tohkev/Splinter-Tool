ee6bfde2afb256159408cb4e97addc0a892bbb21
Kevin <tohkev@gmail.com>
2022-04-03 22:13:58 -0700

    added functionality to switch view

--- frontend/src/cards/components/CardList.js
+++ frontend/src/cards/components/CardList.js
@@ -1,69 +1,21 @@
 import React from "react";
 
 import CardItem from "../components/CardItem";
+import BulkView from "./card-views/BulkView";
+import GridView from "./card-views/GridView";
+import ListView from "./card-views/ListView";
 import "./CardList.css";
 
-function CardList() {
-  const [cards, setCards] = React.useState([]);
-  const [cardDatabase, setCardDatabase] = React.useState([]);
-
-  const cardMap = new Map();
-
-  for (let card of cards) {
-    let cardId = card.card_detail_id;
-    if (cardMap.has(cardId)) {
-      cardMap.set(cardId, {
-        ...cardMap.get(cardId),
-        possess: [
-          ...cardMap.get(cardId).possess,
-          {
-            uid: card.uid,
-            level: card.level,
-            gold: card.gold,
-            delegatedTo: card.delegated_to,
-            onSale: card.market_id,
-          },
-        ],
-      });
-    } else {
-      cardMap.set(cardId, {
-        id: cardId,
-        cardName: cardDatabase.find((item) => item.id === cardId).name,
-        possess: [
-          {
-            uid: card.uid,
-            level: card.level,
-            gold: card.gold,
-            delegatedTo: card.delegated_to,
-            onSale: card.market_id,
-          },
-        ],
-      });
-    }
-  }
-
-  console.log(cardMap);
-
-  React.useEffect(() => {
-    fetch("https://api.splinterlands.io/cards/get_details")
-      .then((res) => res.json())
-      .then((data) => setCardDatabase(data));
-  }, []);
-
-  React.useEffect(() => {
-    fetch("https://api.splinterlands.io/cards/collection/snud")
-      .then((res) => res.json())
-      .then((data) => setCards(data.cards));
-  }, []);
-
-  return (
-    <main>
-      <h2>CardList Component</h2>
-      <ul>
-        <CardItem />
-      </ul>
-    </main>
-  );
+function CardList(props) {
+	if (props.viewMode === "bulkView") {
+		return <BulkView />;
+	} else if (props.viewMode === "gridView") {
+		return <GridView />;
+	} else if (props.viewMode === "listView") {
+		return <ListView />;
+	} else {
+		return <h1>Error showing cards!</h1>;
+	}
 }
 
 export default CardList;
--- frontend/src/cards/pages/UserCards.js
+++ frontend/src/cards/pages/UserCards.js
@@ -8,17 +8,41 @@ import CardList from "../components/CardList";
 import "./UserCards.css";
 
 function UserCards(props) {
+	const [viewMode, setViewMode] = React.useState("bulkView");
+
+	function viewBulk() {
+		setViewMode("bulkView");
+	}
+
+	function viewGrid() {
+		setViewMode("gridView");
+	}
+
+	function viewList() {
+		setViewMode("listView");
+	}
+
 	return (
-		<main className="user-cards">
-			<div className="user-cards__library">
-				<div className="filter-menu-block">
-					<FilterMenu />
+		<React.Fragment>
+			<UserCollectionStats
+				viewBulk={viewBulk}
+				viewGrid={viewGrid}
+				viewList={viewList}
+				viewMode={viewMode}
+			/>
+			<main className="user-cards">
+				<div className="user-cards__library">
+					<div className="filter-menu-block">
+						<FilterMenu />
+					</div>
+					<div className="card-list-block">
+						<main className="card-list">
+							<CardList viewMode={viewMode} />
+						</main>
+					</div>
 				</div>
-				<div className="card-list-block">
-					<CardList />
-				</div>
-			</div>
-		</main>
+			</main>
+		</React.Fragment>
 	);
 }
 
